990d8314136f3b730ec11c3f4ba78e7142a110dd
techtronixx <foxstot@gmail.com>
2024-09-09 01:18:59 +0530

    added scroll animation , fonts , base styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,23 @@ import LaptopContainer from "../src/assets/components/LaptopContainer";
 function App() {
   return (
     <>
-      <Canvas camera={{ fov: 12, position: [0, -10, 220] }}>
-        {/* <OrbitControls /> */}
-        <Environment
-          files={[
-            "https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_08_4k.exr",
-          ]}
-        />
-        <ScrollControls>
-          <LaptopContainer />
-        </ScrollControls>
-      </Canvas>
+      <div className="bg-[#040505] h-full w-screen">
+        <h1 className="absolute top-32 font-bold left-1/2 md:text-4xl lg:text-6xl bg-clip-text bg-gradient-to-b from-neutral-200 via-neutral-600 to-neutral-700 -translate-x-1/2 text-center text-transparent text-6xl">
+          macbook M4 pro 2024.
+        </h1>
+        <Canvas camera={{ fov: 12, position: [0, -10, 220] }}>
+          {/* // Add control movement with mouse or touch */}
+          {/* <OrbitControls /> */}
+          <Environment
+            files={[
+              "https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_08_4k.exr",
+            ]}
+          />
+          <ScrollControls>
+            <LaptopContainer />
+          </ScrollControls>
+        </Canvas>
+      </div>
     </>
   );
 }
--- src/assets/components/LaptopContainer.jsx
+++ src/assets/components/LaptopContainer.jsx
@@ -1,15 +1,21 @@
 /* eslint-disable react/no-unknown-property */
-import { useGLTF, useScroll } from "@react-three/drei";
+import { useGLTF, useScroll, useTexture } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import * as THREE from "three";
 
 const LaptopContainer = () => {
   let model = useGLTF("/mac.glb");
+  let textures = useTexture("/red.jpg");
+
   let meshes = {};
   model.scene.traverse((e) => {
     meshes[e.name] = e;
   });
   meshes.screen.rotation.x = THREE.MathUtils.degToRad(180);
+  meshes.matte.material.map = textures;
+  meshes.matte.material.metalness = 0;
+  meshes.matte.material.emissiveIntensity = 0;
+  meshes.matte.material.roughness = 0;
   let data = useScroll();
 
   useFrame(() => {
